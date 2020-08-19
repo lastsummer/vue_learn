e857d3dcdf2ac6c6bfc2bd46f93fe7bfc6c7c5ac
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2020-08-19 14:45:07 +0800

    加上second project

---
+++ 10-SecondProject/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+     <appQuoteGrid :quotes="quotes"></appQuoteGrid>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue'
+    export default {
+        data: function(){
+            return {
+                maxQuotes:10,
+                quotes:[
+                    'Just a Quote to see something'
+                ]
+            }
+        },
+        components:{
+            appQuoteGrid: QuoteGrid
+        }
+    }
+</script>
+
+<style>
+</style>

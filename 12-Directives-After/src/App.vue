12aaa4929554b92a7fe00db73a295ed11ea03e66
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2020-10-07 13:25:35 +0800

    add Filter init

--- 12-Directives-After/src/App.vue
+++ 12-Directives-After/src/App.vue
@@ -2,8 +2,17 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
-
+                <h1>Built-in Directives</h1>
+                <p v-text="'some text'"></p>
+                <p v-html="'<strong>some text</strong>'"></p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+                <p v-hightlight:background.delayed="'orange'">Color background this</p>
+                <p v-hightlight="'orange'">Color this</p>
             </div>
         </div>
     </div>
@@ -11,6 +20,13 @@
 
 <script>
     export default {
+        directives: {
+            'local-highlight': {
+                bind(el, binding, vnode){
+                    // do something
+                }
+            }
+        }
     }
 </script>
 

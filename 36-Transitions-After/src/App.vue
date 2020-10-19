9c1c1932f9851717ca4b6f90ac29e3e755b135d9
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2020-10-19 13:18:44 +0800

    add transition

--- 36-Transitions-After/src/App.vue
+++ 36-Transitions-After/src/App.vue
@@ -1,24 +1,156 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Animations</h1>
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr />
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br>
+        <button class="btn btn-primary" @click="show = !show">
+          Show Alert
+        </button>
+        <br /><br />
+        <transition :name="alertAnimation">
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
+        <transition name="slide" type="transition">
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
+        <transition 
+          appear
+          enter-active-class="animated shake"
+          leave-active-class="animated flipInY">
+          <div class="alert alert-info" v-if="show">Third</div>
+        </transition>
+        <transition :name="alertAnimation" mode="out-in">
+          <div class="alert alert-info" v-if="show" key="info">This is some Info</div>
+          <div class="alert alert-warning" v-else key="warning">This is some Ｗarning</div>
+        </transition>
+        <hr />
+        <button class="btn btm-primary" @click="load =! load">Load / Remove Element </button>
+        <br><br>
+        <transition
+          @before-enter="beforeEnter"
+          @enter="enter"
+          @after-enter="afterEnter"
+          @enter-cancelled="enterCancelled"
+          @before-leave="beforeLeave"
+          @leave="leave"
+          @after-leave="afterLeave"
+          @leave-cancelled="leaveCancelled"
+          :css="false">
+          <div style="width:300px;height:100px;background-color:lightgreen" v-if="load"></div>
+        </transition>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-
-            }
-        }
+export default {
+  data() {
+    return {
+      show: false,
+      alertAnimation: 'fade',
+      load: true,
+      elementWidth: 100
     }
+  },
+  methods: {
+    beforeEnter(el){
+      console.log('beforeEnter')
+      this.elementWidth = 100
+      el.style.width = this.elementWidth + 'px'
+    },
+    enter(el, done){
+      console.log('enter')
+      let round = 1
+      const interval = setInterval(()=>{
+        el.style.width = (this.elementWidth + round * 10) + 'px'
+        round++
+        if(round > 20){
+          clearInterval(interval)
+          done()
+        }
+      }, 20)
+    },
+    afterEnter(el){
+      console.log('afterEnter')
+    },
+    enterCancelled(el){
+      console.log('enterCancelled')
+    },
+    beforeLeave(el){
+      console.log('beforeLeave')
+      this.elementWidth = 300
+      el.style.width = '300px'
+    },
+    leave(el, done){
+      console.log('leave')
+      let round = 1
+      const interval = setInterval(()=>{
+        el.style.width = (this.elementWidth - round * 10) + 'px'
+        round++
+        if(round > 20){
+          clearInterval(interval)
+          done()
+        }
+      }, 20)
+    },
+    afterLeave(el){
+      console.log('afterLeave')
+    },leaveCancelled(el){
+      console.log('leaveCancelled')
+    }
+  }
+}
 </script>
 
 <style>
+  .fade-enter{
+    opacity:0;
+  }
+  .fade-enter-active{
+    transition: opacity 1s;
+  }
+  .fade-leave{
 
+  }
+  .fade-leave-active{
+    transition: opacity 1s;
+    opacity:0;
+  }
+  .slide-enter{
+    opacity:0;
+  }
+  .slide-enter-active{
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity 1s;
+  }
+  .slide-leave{
+  }
+  .slide-leave-active{
+    animation: slide-out 1s ease-out forwards;
+    transition: opacity 1s;
+    opacity:0;
+  }
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
+  }
 </style>
